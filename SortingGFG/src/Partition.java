6d238fab534f75f4354f0b9c8ed4814cb710b5dc
Nikhil Yedke <nikhilyedke1995@gmail.com>
2021-10-19 08:01:19 +0530

    modified partition and quicksort

--- SortingGFG/src/Partition.java
+++ SortingGFG/src/Partition.java
@@ -1,2 +1,85 @@
-package PACKAGE_NAME;public class Partition {
+import java.util.Arrays;
+
+public class Partition {
+    public static void main(String[] args) {
+        int[] arr = {10,80,30,90,40,50,70};
+        Partition partition = new Partition();
+        System.out.println(partition.partition(arr,0,arr.length-1,5));
+        System.out.println(partition.lPartition(arr,0,arr.length-1));
+        System.out.println(partition.hoarsePartition(arr,0,arr.length-1));
+    }
+
+    //needs pivot as input
+    public int partition(int[] arr, int l, int h, int p){
+        int[] temp = new int[h-l+1];
+        int index = 0;
+        for(int i=l; i<=h; i++){
+            if(arr[i]<arr[p]){
+                temp[index++] = arr[i];
+            }
+        }
+
+        for(int i=l; i<=h; i++){
+            if(arr[i]==arr[p]){
+                temp[index++] = arr[i];
+            }
+        }
+
+        int res = l+index-1;
+        for(int i=l; i<=h; i++){
+            if(arr[i]>arr[p]){
+                temp[index++] = arr[i];
+            }
+        }
+
+        for(int i=l; i<=h; i++){
+            arr[i] = temp[i-l];
+        }
+        return res;
+    }
+
+    //considering pivot is last element
+    //Lomuto partion
+    //if pivot is not in end, move it to end and do partion
+    public int lPartition(int[] arr, int l, int h){
+        int pivot = arr[h];
+        int pIndex = l;
+        for(int i=l; i<h; i++){
+            if(arr[i]<pivot){
+                //swap first
+                swap(arr,i,pIndex);
+                pIndex++;
+            }
+        }
+        swap(arr,pIndex,h);
+        System.out.println(Arrays.toString(arr));
+        return pIndex;
+    }
+
+    //hoarse partition
+    public int hoarsePartition(int[] arr, int l, int h){
+        int pivot = arr[l];
+        int i = l-1, j = h+1;
+        while(true){
+            do {
+                i++;
+            }while (arr[i]<pivot);
+
+            do {
+                j--;
+            }while (arr[j]>pivot);
+
+            if(i>=j) {
+                System.out.println(Arrays.toString(arr));
+                return j;
+            }
+            swap(arr,i,j);
+        }
+    }
+
+    private static void swap(int[] a, int i, int pIndex) {
+        int temp = a[i];
+        a[i] = a[pIndex];
+        a[pIndex] = temp;
+    }
 }
--- SortingGFG/src/QuickSort.java
+++ SortingGFG/src/QuickSort.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class QuickSort {
+import java.util.Arrays;
+
+public class QuickSort {
+
+    private Partition partition = new Partition();
+
+    public static void main(String[] args) {
+        QuickSort quickSort = new QuickSort();
+        int[] arr = {8,4,7,9,3,10,5};
+        quickSort.sortUsingLomutoAlgorithm(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public void sortUsingLomutoAlgorithm(int[] arr,int low, int high){
+        if(low<high){
+            int p = partition.lPartition(arr,low,high);
+            sortUsingLomutoAlgorithm(arr,low,p-1);
+            sortUsingLomutoAlgorithm(arr,p+1,high);
+        }
+    }
+
+    public void sortUsingHoarsePartition(int[] arr, int l, int h){
+        if(l<h){
+            int p = partition.hoarsePartition(arr,l,h);
+            sortUsingHoarsePartition(arr,l,p);
+            sortUsingHoarsePartition(arr,p+1,h);
+        }
+    }
 }
